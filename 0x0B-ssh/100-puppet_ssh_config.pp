9a8dbbff8820a83702b3b78da5bc0823640d2785
safiy-safy <137833469+safiy-safy@users.noreply.github.com>
2024-03-25 08:07:48 -0700

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
-# Using puppet to connect without password
-
-file { '/etc/ssh/ssh_config':
-  ensure => present,
+# Puppet script to create ssh config file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
-file _line { 'Turn off passwd auth':
-  path     => '/etc/ssh/ssh_config',
-  line     => 'PasswordAuthentication no',
-  match    => '^#PasswordAuthentication',
-}
-
-file_line { 'Declare identity file ':
-  path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^#IdentityFile',
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
